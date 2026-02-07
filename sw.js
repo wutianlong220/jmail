0302889db65dc7c3cd86fade90bb77a2a1d5a045
wutianlong220 <37255869+wutianlong220@users.noreply.github.com>
2026-02-07 23:29:36 +0800

    添加monetag广告

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10576128
+    "zoneId": 10581846
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
